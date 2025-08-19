a7a134c0160c43a28c7e7e3d7f71c19bd3d12f30
siddiq73si <sinthasiddiq@gmail.com>
2025-08-19 11:42:07 +0530

    Boolean file

---
+++ boolean.java
@@ -0,0 +1,11 @@
+class BooleanCheck
+{	
+	public static void main(String args[])
+	{
+			boolean a=true;
+			boolean b=false;
+			int c=50;
+			int d=20;
+			System.out.println("!c<d = "+!(c<d));
+	}
+}
